eeab01125be0164c2af2bf9f483198ac6016f1cc
Amiya Ashutosh Sahu <79083019+Amiya-10@users.noreply.github.com>
2022-01-31 16:09:36 +0530

    Add files via upload
    This is assignment on inheritance

---
+++ Building.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_assignment
+{
+    class Building
+    {
+        public string type;
+        public string capacity;
+        public string dimension;
+
+        public Building(string type,string capacity,string dimension)
+        {
+            this.type = type;
+            this.capacity = capacity;
+            this.capacity = dimension;
+
+        }
+        public virtual void Showdata()
+        {
+            Console.WriteLine("The type of building is{0}", type);
+            Console.WriteLine("The capacity is {0}", capacity);
+            Console.WriteLine("The Dimension is: " + dimension);
+        }
+    }
+    class Flat : Building
+    {
+        public int flatno;
+        public string address;
+
+        public Flat(string type, string capacity, string dimension, int flatno, string address) : base(type, capacity, dimension)
+        {
+            if (type == "Flat")
+            {
+                if (capacity == "2BHK")
+                {
+                    Console.WriteLine("The rate is: 5000$");
+                }
+                if (capacity == "3BHK")
+                {
+                    Console.WriteLine("The rate is: 6000$");
+                }
+                if (capacity == "4BHK")
+                {
+                    Console.WriteLine("The rate is: 7000$");
+                }
+            }
+        }
+        public override void Showdata()
+        {
+            Console.WriteLine("The Flat no. is {0}", flatno);
+            Console.WriteLine("The address is {0}", address);
+            Console.WriteLine("The capacity is {0}", capacity);
+
+        }
+    }
+        
+    class Villa : Building
+    {
+          public Villa(string type,string capacity, string dimension) : base(type,capacity,dimension) 
+          {
+              if (type == "Villa")
+              {
+                  Console.WriteLine("Enter the size(2BHK/3BHK/4BHK):");
+                  Console.ReadLine();
+                  if (capacity == "2BHK")
+                  {
+                      Console.WriteLine("The rate is: 8000$");
+                  }
+                  if (capacity == "3BHK")
+                  {
+                      Console.WriteLine("The rate is: 9000$");
+                  }
+                  if (capacity == "4BHK")
+                  {
+                      Console.WriteLine("The rate is: 10000$");
+                  }
+              }
+          }
+        public override void Showdata()
+        {
+            Console.WriteLine("The Villa is of {0}",capacity );
+            
+        }
+    }
+    
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Building k = new Building("Flat","2BHK","20x30x40");
+            k.Showdata();
+            Building m = new Flat("Flat","2BHK","20x20x20",103,"abcd");
+            m.Showdata();
+            Console.ReadKey();
+        }
+    }
+    
+
+}
--- Program.cs
+++ Program.cs
@@ -4,97 +4,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment_on_classes
+namespace Inheritance_assignment
 {
-    class Account
+    class Furniture
     {
+        public int order_id;
+        public DateTime orderdate = new DateTime(2015, 12, 31);
+        public string furniture_type;
+        public int quantity;
+        public double amount;
 
-        private double balance = 100000;
-        public double bal
+    
+        public void Getdata()
         {
-            get { return balance; }
-            set { balance = value; }
+            Console.WriteLine("Enter the order id:");
+            order_id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter furniture type:");
+            Console.ReadLine();
+            Console.WriteLine("Enter the quantity:");
+            quantity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the amount:");
+            amount = Convert.ToDouble(Console.ReadLine());
         }
-        class fuctions
+        public void Details()
         {
-            Account i = new Account();
-            string name;
-            int account;
-            double withdraw, dep, tobal;
-
-            public void fun1()
+            Console.WriteLine("The Order Id: " + order_id);
+            Console.WriteLine("The Order Date: " + orderdate);
+            Console.WriteLine("The Furniture type: " + furniture_type);
+            Console.WriteLine("Quantity: " + quantity);
+            Console.WriteLine("Amount: " + amount);
+        }
+    }
+    class Chair : Furniture
+    {
+        public string chairtype;
+        public string purpose;
+        public string woodtype;
+        public string steeltype;
+        public string color;
+        
+        new public void Getdata()
+        {
+            if (furniture_type == "Chair")
             {
-                Console.WriteLine("Enter the name of depositor");
-                name = Console.ReadLine();
-                Console.WriteLine("Enter the account no.");
-                account = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter the deposit amount");
-                dep = Convert.ToDouble(Console.ReadLine());
-                tobal = i.bal + dep;
-                Console.WriteLine("Name of Depositor" + name);
-                Console.WriteLine("Account no." + account);
-                Console.WriteLine("Total balance in account after deposit" + tobal);
-
-            }
-
-            public void fun2()
-            {
-
-                Console.WriteLine("Enter Account Name:");
-                name = Console.ReadLine();
-                Console.WriteLine("Enter Account Number:");
-                account = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Withdraw Amount:");
-                withdraw = Convert.ToDouble(Console.ReadLine());
-                if (withdraw <= i.bal)
+                Console.WriteLine("Enter the Chair type(Wood/Steel/Plastic): " + chairtype);
+                Console.ReadLine();
+                Console.WriteLine("Enter the purpose of use (Home/Office): " + purpose);
+                Console.ReadLine();
+                if (chairtype == "Wood")
                 {
-                    tobal = i.bal - withdraw;
-                    Console.WriteLine("Account Name" + name);
-                    Console.WriteLine("Account Number" + account);
-                    Console.WriteLine("Total balance after withdrawal" + tobal);
-
+                    Console.WriteLine("please select (Teak Wood/Oak Wood): " + woodtype);
+                    Console.ReadLine();
+                    Console.WriteLine("MRP: 40$");
+                }
+                if (chairtype == "Steel")
+                {
+                    Console.WriteLine("Please select color(Grey steel/Green steel/Brown steel): " + steeltype);
+                    Console.ReadLine();
+                    Console.WriteLine("MRP: 34$");
+                }
+                if (chairtype == "Plastic")
+                {
+                    Console.WriteLine("Please select a color(Green/Red/Blue/White): " + color);
+                    Console.ReadLine();
+                    Console.WriteLine("MRP: 20$");
                 }
                 else
-                    Console.WriteLine("Withdraw amount doesn't exist in your account");
-
-            }
-
-        }
-        class Program
-        {
-            static void Main(string[] args)
-            {
-                char agn;
-                do
                 {
-                    fuctions k = new fuctions();
-                    int num;
-                    Console.WriteLine("\nPress 1 for Deposit an Amount. \nPress 2 for Withdraw an Amount.");
-                    num = Convert.ToInt32(Console.ReadLine());
-                    switch (num)
-                    {
-
-                        case 1:
-
-                            k.fun1();
-                            break;
-
-                        case 2:
-
-                            k.fun2();
-                            break;
-
-                        default:
-
-                            Console.WriteLine("Invalid Selection!!!");
-                            break;
-
-                    }
-                    Console.WriteLine("\nDo you want to continue this program ? (y / n)");
-
-                    agn = Convert.ToChar(Console.ReadLine());
-                } while (agn == 'y');
+                    Console.WriteLine("Enter correct info");
+                }
             }
+        }
+        
+
+    }
+    class Cot : Furniture
+    {
+        public string cot_type;
+        public string capacity;
+        public string Woodtype;
+        public string Steeltype;
+
+        new public void Getdata()
+        {
+            if (furniture_type == "Cot")
+            {
+                Console.WriteLine("Enter Cot type(Wood/Steel): " + cot_type);
+                Console.ReadLine();
+                Console.WriteLine("Enter the capacity(single/double): " + capacity);
+                if (cot_type == "Wood")
+                {
+                    Console.WriteLine("please select (Teak Wood/Oak Wood): " + Woodtype);
+                    Console.ReadLine();
+                    Console.WriteLine("You have selected{0} type", Woodtype);
+                    Console.WriteLine("MRP: 60$");
+                }
+                if (cot_type == "Steel")
+                {
+
+                    Console.WriteLine("Please select color(Grey steel/Green steel/Brown steel): " + Steeltype);
+                    Console.ReadLine();
+                    Console.WriteLine("You have selected{0} type", Steeltype);
+                    Console.WriteLine("MRP: 89$");
+                }
+            }
+        }
+    }
+    
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Furniture k = new Furniture();
+            k.Getdata();
+            k.Details();
+            Console.ReadKey();
+
         }
     }
 }
